db81169faa92c142a68f0ecdf269aff1a627a1c1
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2022-12-19 15:51:26 +0530

    Simplify env var

--- internal/config/spec.go
+++ internal/config/spec.go
@@ -13,7 +13,7 @@ type Spec struct {
 	Host                string        `default:"127.0.0.1"`
 	CrtUri              string        `envconfig:"CRT_URI" default:"crt.pem"`
 	KeyUri              string        `envconfig:"KEY_URI" default:"key.pem"`
-	Namespace           uuid.UUID     `envconfig:"BFID_NAMESPACE"`
+	Namespace           uuid.UUID     `envconfig:"NAMESPACE"`
 	MetricsPushUrl      string        `envconfig:"METRICS_PUSH_URL"`
 	MetricsPushInterval time.Duration `envconfig:"METRICS_PUSH_INTERVAL"`
 }
