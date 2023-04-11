03ccb71f94a8f79021d4622ee5ab932a86ba6fb4
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2023-04-12 00:12:40 +0530

    Configurable log level
    Default to JSON log output with configurable level.

--- internal/config/spec.go
+++ internal/config/spec.go
@@ -2,14 +2,16 @@ package config
 
 import (
 	"github.com/google/uuid"
+	"golang.org/x/exp/slog"
 )
 
 const Prefix = "BF"
 
 // Spec configures a bifrost server.
 type Spec struct {
-	Address   string    `envconfig:"ADDR" default:"127.0.0.1:8080"`
-	CrtUri    string    `envconfig:"CRT"  default:"crt.pem"`
-	KeyUri    string    `envconfig:"KEY"  default:"key.pem"`
-	Namespace uuid.UUID `envconfig:"NS"   default:"1512daa4-ddc1-41d1-8673-3fd19d2f338d"`
+	LogLevel  slog.Level `envconfig:"LOG_LEVEL" default:"info"`
+	Address   string     `envconfig:"ADDR"      default:"127.0.0.1:8080"`
+	CrtUri    string     `envconfig:"CRT"       default:"crt.pem"`
+	KeyUri    string     `envconfig:"KEY"       default:"key.pem"`
+	Namespace uuid.UUID  `envconfig:"NS"        default:"1512daa4-ddc1-41d1-8673-3fd19d2f338d"`
 }
