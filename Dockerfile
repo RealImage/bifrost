daaca17e82180bf81adf21507f205275124ad31a
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2023-04-13 00:20:37 +0530

    License under MPL-2.0

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 ARG GO_VERSION="1.20"
 
 FROM docker.io/library/golang:${GO_VERSION} as builder
--- internal/cafiles/cafiles.go
+++ internal/cafiles/cafiles.go
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 // cafiles can fetch CA certificate and private key PEM files from many storage backends.
 // PEM encoded CA files can be fetched from local filesystem, AWS S3, or AWS Secrets Manager.
 package cafiles
