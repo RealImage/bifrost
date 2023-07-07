2f928f60caec56aeb9f27d2b250c17b6f3f5bd4e
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2023-07-07 21:32:20 +0530

    bifrost.HTTPClient accepts a pool of RootCAs

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package bifrost
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -25,11 +26,10 @@ func (s singleHostRoundTripper) RoundTrip(r *http.Request) (*http.Response, erro
 	return s.transport.RoundTrip(r)
 }
 
-// HTTPClient returns a HTTP client set up for mTLS with the provided api URL.
-// The returned client will only send requests to the api URL host.
-// The request path will be joined with the api URL path, if any.
-// The client will use the provided TLS client certificate to identify itself.
-func HTTPClient(apiUrl string, clientCert *tls.Certificate) (*http.Client, error) {
+// HTTPClient returns a http.Client configured for mTLS with apiUrl.
+// If clientCert is not nil, it will be used for client authentication.
+// If rootCAs is not nil, it will be used to verify the server certificate.
+func HTTPClient(apiUrl string, clientCert *tls.Certificate, rootCAs *x509.CertPool) (*http.Client, error) {
 	rt := &singleHostRoundTripper{
 		transport: http.DefaultTransport,
 	}
@@ -46,6 +46,7 @@ func HTTPClient(apiUrl string, clientCert *tls.Certificate) (*http.Client, error
 			ForceAttemptHTTP2: true,
 			TLSClientConfig: &tls.Config{
 				Certificates: []tls.Certificate{*clientCert},
+				RootCAs:      rootCAs,
 			},
 		}
 	}
