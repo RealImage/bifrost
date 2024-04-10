0c6f15967db57a086a970f364bec9df74b5e017f
Ananth Bhaskararaman <ananth.b@qubecinema.com>
2024-04-10 15:09:27 +0530

    feat: Fetch cert on client creation itself
    Fetch a client certificate as soon as the client as created.
    This speeds up future connections and catches errors early.

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package bifrost
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
 	"io"
@@ -11,8 +12,7 @@ import (
 )
 
 // HTTPClient returns a http.Client set up for TLS Client Authentication (mTLS).
-// The client will request a new certificate from the given URL for the given namespace
-// using the given private key when needed.
+// The client will request a new certificate from the bifrost caUrl when needed.
 // If roots is not nil, then only those Root CAs are used to authenticate server certs.
 // If ssllog is not nil, the client will log TLS key material to it.
 func HTTPClient(
@@ -21,11 +21,14 @@ func HTTPClient(
 	privkey *PrivateKey,
 	roots *x509.CertPool,
 	ssllog io.Writer,
-) *http.Client {
+) (*http.Client, error) {
 	cr := &certRefresher{
 		url:     caUrl,
 		ns:      ns,
 		privkey: privkey,
+	}
+	if _, err := cr.GetClientCertificate(nil); err != nil {
+		return nil, err
 	}
 	tlsConfig := &tls.Config{
 		GetClientCertificate: cr.GetClientCertificate,
@@ -36,7 +39,7 @@ func HTTPClient(
 	tlsTransport.TLSClientConfig = tlsConfig
 	return &http.Client{
 		Transport: tlsTransport,
-	}
+	}, nil
 }
 
 type certRefresher struct {
@@ -49,13 +52,26 @@ type certRefresher struct {
 func (cr *certRefresher) GetClientCertificate(
 	info *tls.CertificateRequestInfo,
 ) (*tls.Certificate, error) {
+	ctx := context.Background()
+	if info != nil {
+		ctx = info.Context()
+	}
 	// If the certificate is nil or is going to expire soon, request a new one.
 	if cr.cert == nil || cr.cert.NotAfter.Before(time.Now().Add(-time.Minute*10)) {
-		cert, err := RequestCertificate(info.Context(), cr.url, cr.ns, cr.privkey)
+		cert, err := RequestCertificate(ctx, cr.url, cr.ns, cr.privkey)
 		if err != nil {
 			return nil, err
 		}
 		cr.cert = cert
 	}
-	return X509ToTLSCertificate(cr.cert.Certificate, cr.privkey.PrivateKey), nil
+
+	tlsCert := X509ToTLSCertificate(cr.cert.Certificate, cr.privkey.PrivateKey)
+
+	if info != nil {
+		if err := info.SupportsCertificate(tlsCert); err != nil {
+			return nil, err
+		}
+	}
+
+	return tlsCert, nil
 }
